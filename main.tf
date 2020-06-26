24ddfd460edcae30f26db3e31e9e085943a97ad9
Cristian Alejandro Ramirez Holguin <cristian.ramirez3@ibm.com>
2020-06-26 12:41:31 -0500

    Upload files

---
+++ main.tf
@@ -0,0 +1,26 @@
+
+
+
+resource "ibm_lbaas" "lbaas" {
+  name        = "test"
+  description = "updated desc-used for terraform uat"
+  subnets     = [1492041]
+ # datacenter  = "dal13"
+
+  protocols {
+    frontend_protocol     = "HTTP"
+    frontend_port         = 80
+    backend_protocol      = "HTTP"
+    backend_port          = 80
+    load_balancing_method = "round_robin"
+    
+  }
+}
+
+resource "ibm_lbaas_server_instance_attachment" "lbaas_member" {
+  count              = 1
+  private_ip_address = "10.95.58.114"
+  weight             = 40
+  lbaas_id           = "${ibm_lbaas.lbaas.id}"
+}
+
